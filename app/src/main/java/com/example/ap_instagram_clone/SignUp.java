7e2278f866da28c9a821600d84b516ebc1dfaadf
Gideon <gideonovuzorie@gmail.com>
2019-07-23 18:50:13 +0100

    Implemented the Sign up Functionality

--- app/src/main/java/com/example/ap_instagram_clone/SignUp.java
+++ app/src/main/java/com/example/ap_instagram_clone/SignUp.java
@@ -2,47 +2,70 @@ package com.example.ap_instagram_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class SignUp extends AppCompatActivity {
+
+    private EditText sign_up_email,sign_up_username,sign_up_password;
+    private Button btnSignup,btn_Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        //ParseInstallation.getCurrentInstallation().saveInBackground();
+        setTitle("Sign Up");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        sign_up_email=findViewById(R.id.txtSignUpEmail);
+        sign_up_username=findViewById(R.id.txtSignUpUsername);
+        sign_up_password=findViewById(R.id.txtLoginPassword);
+
+        btn_Login=findViewById(R.id.btnLogin);
+        btnSignup=findViewById(R.id.btnSignUp);
+
+
 
     }
 
-    public void textClicked(View view){
+    public void Login(View view){
 
-        try {
-            final ParseObject boxer = new ParseObject("Boxer");
-            boxer.put("punch_speed", 200);
+        Intent intent=new Intent(SignUp.this,LoginActivity.class);
+        startActivity(intent);
+    }
+    public void SignUp(View view){
 
-            boxer.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if (e == null) {
-                        FancyToast.makeText(SignUp.this, "The Boxer Object is saved", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
-                    } else {
-                        Log.d("ERROR", e.getLocalizedMessage());
-                    }
+        final ParseUser appUser=new ParseUser();
+        appUser.setEmail(sign_up_email.getText().toString());
+        appUser.setUsername(sign_up_username.getText().toString());
+        appUser.setPassword(sign_up_password.getText().toString());
+
+        appUser.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e==null){
+                    FancyToast.makeText(SignUp.this,appUser.getUsername()+" signed up successfully",
+                            FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+                }else {
+                    FancyToast.makeText(SignUp.this,"There was an Error! "+e.getLocalizedMessage(),
+                            FancyToast.LENGTH_LONG,FancyToast.ERROR,false).show();
                 }
-            });
-        }
-        catch (Exception ex){
-            Log.d("ERROR", ex.getLocalizedMessage());
-        }
+            }
+        });
     }
 }
